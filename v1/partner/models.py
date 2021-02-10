2770cba04b5cd334214c228bd95ab8e81488f71d
dhakalkushal <55182298+dhakalkushal@users.noreply.github.com>
2021-02-10 21:42:42 +0545

    added status choices in partner table

--- v1/partner/models.py
+++ v1/partner/models.py
@@ -6,7 +6,15 @@ from v1.shop.models import Shop, ShopBranch
 from v1.user.models import User
 
 # Food Category model
-class Partner(models.Model):
+class Partner(models.Model): 
+  STATUS_CHOICES = [
+    (0, 'New'),
+    (1, 'Documents Requested'),
+    (2, 'Approved'),
+    (3, 'Cancelled'),
+    (4, 'Rejected'),
+]
+
   uuid = models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True)
   title = models.CharField(max_length=255)
   address = models.CharField(max_length=255)
@@ -14,6 +22,8 @@ class Partner(models.Model):
   last_name = models.CharField(max_length=255)
   email = models.EmailField()
   phone = models.CharField(max_length=100)
+  status = models.IntegerField(choices=STATUS_CHOICES, default=0)
+  
       
   def __str__(self):
     return self.title 
