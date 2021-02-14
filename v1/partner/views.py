23e29e45b10e24d564eda5193084d2aafc9d2ff8
dhakalkushal <55182298+dhakalkushal@users.noreply.github.com>
2021-02-14 16:00:58 +0545

    not everyone can add status in partner

--- v1/partner/views.py
+++ v1/partner/views.py
@@ -3,16 +3,20 @@ from rest_framework import viewsets
 from .models import Partner
 from v1.third_party.rest_framework.permissions import IsStaff
 from rest_framework.permissions import AllowAny
-from .serializers import PartnerSerializer
+from .serializers import PartnerSerializer, PartnerCreateSerializer
 
 
 class PartnerViewSet(viewsets.ModelViewSet):
     queryset = Partner.objects.all()
     serializer_class = PartnerSerializer
-    permission_classes = [IsStaff]
+    
+    def get_serializer_class(self):
+        if self.request.user.is_staff:
+            return PartnerSerializer
+        return PartnerCreateSerializer
 
     def get_permissions(self):
-        if self.action == 'list':
+        if self.action == 'create':
             return [AllowAny(), ]
         else:
             return [IsStaff(), ]
