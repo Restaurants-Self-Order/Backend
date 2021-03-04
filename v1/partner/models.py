b74db4ec9ab003ee8d190dd18ffbd3c71411a472
SazidAhmed <sazidahmed.official@gmail.com>
2021-03-04 21:22:26 +0600

    street_address changed to detailed_address

--- v1/partner/models.py
+++ v1/partner/models.py
@@ -51,7 +51,7 @@ class Partner(models.Model):
     email = models.EmailField()
     phone = models.CharField(max_length=100)
 
-    street_address = models.CharField(max_length=255)
+    detailed_address = models.CharField(max_length=255)
     state = models.CharField(max_length=255)
     city = models.CharField(max_length=255)
     country = models.ForeignKey(Country, on_delete=models.DO_NOTHING)
