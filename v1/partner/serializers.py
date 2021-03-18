7b8751065722422ee5f9c266d09a52edb8d99b68
Sazid Ahmed <39996870+SazidAhmed@users.noreply.github.com>
2021-03-18 06:35:19 +0600

    Update serializers.py

--- v1/partner/serializers.py
+++ v1/partner/serializers.py
@@ -7,7 +7,7 @@ class PartnerSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Partner
-        fields = ('uuid', 'first_name', 'last_name', 'email', 'phone', 'detailed_address', 'state' 'city', 'country', 'shop_name', 'created_at', 'updated_at')
+        fields = ('uuid', 'first_name', 'last_name', 'email', 'phone', 'detailed_address', 'state', 'city', 'country', 'shop_name', 'created_at', 'updated_at')
         read_only_fields = 'created_at', 'updated_at'
 
 
